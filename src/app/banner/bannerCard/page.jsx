7960402e9ff303b90c3371fef4a3d2c8030d91bb
saifsammy <saifsammy653@gmail.com>
2024-02-23 23:03:40 +0600

    banner component still pending

--- src/app/banner/bannerCard/page.jsx
+++ src/app/banner/bannerCard/page.jsx
@@ -12,10 +12,10 @@ const imgArray = [
 console.log(imgArray);
 const BannerCard = () => {
     return (
-        <div className='mt-10 flex flex-col lg:flex-row justify-center items-center gap-20'>
+        <div className='mt-5 flex flex-col lg:flex-row justify-center items-center gap-10 lg:gap-20'>
 
-            {imgArray.map((content, index) => (<div key={index} className='flex flow-row lg:flex-col items-center gap-5'>
-                <Image src={content.img} alt='' />
+            {imgArray.map((content, index) => (<div key={index} className='flex flow-row lg:flex-col justify-around lg:justify-center items-center gap-5'>
+                <Image className='w-32 lg:w-full p-2' src={content.img} alt='images' />
                 <div className='flex flex-col text-left lg:text-center'>
                     <h1 className='text-xl font-bold'>{content?.title}</h1>
                     <span>{content?.sub}</span>
