99cf46281de7ecbbd7b15d370d0bbf49907529b9
Nahidahmmed <nahidahmmed411@gmail.com>
2024-02-24 22:52:18 +0600

    Sponsors componenet is ready

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Robin from "./robin/page";
 import Banner from "./banner/page";
 import Slogan from "./slogan/page";
 import PropertyType from "./propertyType/page";
+import Sponsors from "@/components/Sponsors/Sponsors";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <Banner />
       <Slogan />
       <PropertyType />
+      <Sponsors/>
       {/* <Robin /> */}
     </>);
 };
