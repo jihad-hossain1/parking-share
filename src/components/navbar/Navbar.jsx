a736105c6670bb65788619678a5594c4267bd8a0
Pujon Das Auvi <auvipujondas@gmail.com>
2024-02-25 20:07:32 +0600

    navbar updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { ThemeSwitchButton } from "./themeSwitch/ThemeSwitchButton";
+import MobileNav from "../MobileNav/MobileNav";
 
 const Navbar = () => {
   return (
@@ -8,14 +9,18 @@ const Navbar = () => {
      <div className="container mx-auto py-2 shadow-sm sticky top-0 backdrop-blur-md">
       <div className="flex justify-between items-center">
         <div>Parking Share</div>
-        <div className="flex items-center gap-10">
+        <div className="lg:flex items-center gap-10 hidden">
           <Link href={"/"}>Home</Link>
           <Link href={"/"}>About</Link>
           <Link href={"/"}>Become Client</Link>
         </div>
-        <div>
+        <div className='flex items-center justify-center' >
           <ThemeSwitchButton />
+          <div className='block lg:hidden' >
+          <MobileNav/>
         </div>
+        </div>
+
       </div>
     </div>
  </div>
